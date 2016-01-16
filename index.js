7d40d9ea11bc75ca98454a642530e116b3c9f1c2
Arata Tanaka <tarata43@yahoo.co.jp>
2016-01-16 17:50:55 +0900

    Fix main script

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ Fs   = require('fs');
 Path = require('path');
 
 module.exports = function(robot) {
-  var path = Path.resolve(__dirname, 'src/scripts');
+    var path = Path.resolve(__dirname, 'src/scripts');
     Fs.exists(path, function(exists) {
         if (exists) {
-            for (var file in Fs.readdirSync(path)) {
-                robot.loadFile(path, file);
-            }
+            Fs.readdirSync(path).forEach(function(f) {
+                robot.loadFile(path, f);
+            });
         }
     });
 }
